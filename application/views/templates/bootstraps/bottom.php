4238ee174bef61ed065243e668b2a64d1a55a44e
Tera Byte <tubagus.aom.swk@gmail.com>
2024-12-22 21:11:05 +0700

    mergeupdate

--- application/views/templates/bootstraps/bottom.php
+++ application/views/templates/bootstraps/bottom.php
@@ -2,18 +2,26 @@
       <footer class="section footer-classic bg-default">
         <div class="container">
           <div class="row row-15">
-            <div class="col-sm-6">
-              <p class="rights"><span>MitraOne TV</span><span>&nbsp;</span><span>&copy;&nbsp;</span><span class="copyright-year"></span><span>&nbsp;</span><span>\&nbsp;</span>All Rights Reserved
-                \ Design by <a href="https://www.terabytee.my.id/">terabytee</a>
+            <div class="col-sm-12">
+              <p class="rights">
+                <span>MitraOne TV</span>
+                <!-- <span>&nbsp;</span> -->
+                <span>&copy;</span>
+                <span class="copyright-year"></span>
+                <!-- <span>&nbsp;</span> -->
+                <!-- <span>&nbsp;-&nbsp;</span>
+                <span>Design by <a href="https://www.terabytee.my.id/" target="_blank">terabytee</a></span> -->
+                <!-- <a href="https://www.terabytee.my.id/">terabytee</a> -->
+                <span style="float: right;">Design by <a href="https://www.terabytee.my.id/" target="_blank">terabytee</a></span>
               </p>
             </div>
-            <div class="col-sm-6">
+
+            <!-- <div class="col-sm-4">
               <div class="footer-contact">
-                <!-- <a href="contacts.html"> -->
-                <a href="#">
-                  <div class="icon novi-icon mdi mdi-email-outline"></div>Contact Me</a></div>
-            </div>
-          </div>
+                <span>&nbsp;-&nbsp;</span>
+                <span>Design by <a href="https://www.terabytee.my.id/" target="_blank">terabytee</a></span>
+              </div>
+            </div> -->
         </div>
       </footer>
     </div>
@@ -32,8 +40,6 @@
   <script>
     // var player_1 = videojs("hls-example-1");
     // player_1.play();
-
-
   </script>
 
 
