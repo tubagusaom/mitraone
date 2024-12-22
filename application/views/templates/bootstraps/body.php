9ef5063126070f6b5fb7ac2ab14711190b515d83
Tera Byte <tubagus.aom.swk@gmail.com>
2024-12-22 20:42:39 +0700

    mergeupdate

--- application/views/templates/bootstraps/body.php
+++ application/views/templates/bootstraps/body.php
@@ -84,6 +84,7 @@
                       width="2000"
                       height="400"
                       controls
+                      autoplay
                       poster="<?=base_url()?>assets_tv/images/tv/poster_blue_idtv.png"
                     >
                       <source
