061468267070a2c2dd7683b523f6a9b93db1a8c2
Tera Byte <tubagus.aom.swk@gmail.com>
2024-12-29 05:49:40 +0700

    mergeupdate

--- application/views/templates/bootstraps/header.php
+++ application/views/templates/bootstraps/header.php
@@ -35,7 +35,7 @@
 	<!-- Theme Custom CSS -->
     <link rel="stylesheet" href="<?php echo base_url() ?>assets_tv/css/bootstrap.css">
     <link rel="stylesheet" href="<?php echo base_url() ?>assets_tv/css/fonts.css">
-    <link rel="stylesheet" href="<?php echo base_url() ?>assets_tv/css/style.min.css">
+    <link rel="stylesheet" href="<?php echo base_url() ?>assets_tv/css/style.tb.css">
 
     <!-- <style>
 		.ie-panel{display: none;background: #212121;padding: 10px 0;box-shadow: 3px 3px 5px 0 rgba(0,0,0,.3);clear: both;text-align:center;position: relative;z-index: 1;} html.ie-10 .ie-panel, html.lt-ie-10 .ie-panel {display: block;}
