4788a9bb8a433c79c1378b4c4518457fd9d08df2
Tera Byte <tubagus.aom.swk@gmail.com>
2025-01-07 16:16:46 +0700

    mergeupdate

--- application/views/templates/bootstraps/embed_video.php
+++ application/views/templates/bootstraps/embed_video.php
@@ -19,34 +19,35 @@
 
     <style>
         .videoContainer {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        left: 0;
-        bottom: 0;
-        right: 0;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background-color: #222d4f;
-    }
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+            bottom: 0;
+            right: 0;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-color: #222d4f;
+        }
 
-    iframe {
-        /* optional */
-        width: 98%;
-        height: 97%;
-        padding: 8px;
-        border-radius: 15px;
-    }
+        iframe {
+            /* optional */
+            width: 95%;
+            height: 97%;
+            padding: 8px;
+            border-radius: 15px;
+        }
     </style>
 </head>
 
 <body>
     <div class="videoContainer">
         <!-- <img class="img_poster" src="<?=$value_videos->poster_video?>" alt="<?=$value_videos->nama_video?>" title="<?=$value_videos->nama_video?>"/> -->
-        <iframe class="videoContainer__video" width="1920" height="1080" src="https://www.youtube.com/embed/<?=$id_embed?>?modestbranding=1&autoplay=1&controls=0&fs=0&loop=1&rel=0&showinfo=0&disablekb=1&playlist=<?=$id_embed?>" frameborder="0"></iframe>
+        <iframe class="videoContainer__video" width="100%" height="100%" src="https://www.youtube.com/embed/<?=$id_embed?>?autoplay=1&mute=1&modestbranding=1&controls=1&fs=0&loop=1&rel=0&showinfo=0&disablekb=1&playlist=<?=$id_embed?>" frameborder="0">
+        </iframe>
         
             
     </div>    
